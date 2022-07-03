d5dc023571fa0f4ce4ce1f17f3113c479942b0db
staniewskib <staniews@student.agh.edu.pl>
2022-07-03 22:27:52 +0200

    add inputs/outputs

--- rtl/axi_lite_memory.v
+++ rtl/axi_lite_memory.v
@@ -1,7 +1,34 @@
-module axi_lite_memory (
 
+module axi_lite_memory #(
+	parameter	AXIL_DATA_WIDTH = 32,
+				AXIL_ADDR_WIDTH = 4
+)(
+	input wire							clk,
+	input wire							reset,
+	
+	//AXI4-Lite read signals
+	input wire							arvalid,
+	input wire [AXIL_ADDR_WIDTH-1:0]	araddr,
+	input wire							rready,
+	
+	output reg							arready,
+	output reg							rvalid,
+	output reg [AXIL_DATA_WIDTH-1:0]	rdata,
+	output reg [1:0]					rresp,
+	
+	//AXI4-Lite write signals
+	input wire							awvalid,
+	input wire [AXIL_ADDR_WIDTH-1:0]	awaddr,
+	input wire							wvalid,
+	input wire [AXIL_DATA_WIDTH-1:0]	wdata,
+	input wire [AXIL_DATA_WIDTH/8-1:0]	wstrb,
+	input wire							bready,
+
+	output reg							awready,
+	output reg							wready,
+	output reg							bvalid,
+	output reg [1:0]					bresp
 );
 
+
 endmodule
-
-
